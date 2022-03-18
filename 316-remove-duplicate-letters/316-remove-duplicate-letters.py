b41ee2e7efe4768e83cb32b51160d0639b541620
Jeet Mehta <47302941+JeetMehta99@users.noreply.github.com>
2022-03-17 22:10:55 -0500

    Time: 47 ms (68.10%), Space: 14 MB (54.66%) - LeetHub

--- 316-remove-duplicate-letters/316-remove-duplicate-letters.py
+++ 316-remove-duplicate-letters/316-remove-duplicate-letters.py
@@ -1,34 +1,34 @@
 class Solution:
     def removeDuplicateLetters(self, s: str) -> str:
-        dic = {}
-        for i in range(len(s)):
-            dic[s[i]] = i
-            
-        stack_Arr = []
-        visited = set()
-        for i in range(len(s)):
-            if s[i] in visited:
-                continue
-            while stack_Arr and s[i] < stack_Arr[-1] and dic[stack_Arr[-1]] > i:
-                visited.remove(stack_Arr[-1])
-                stack_Arr.pop()
-                
-            stack_Arr.append(s[i])
-            visited.add(s[i])
-        return "".join(stack_Arr)
-        
-#         arr = []
-    
+#         dic = {}
 #         for i in range(len(s)):
-#             if s[i] in arr:
+#             dic[s[i]] = i
+            
+#         stack_Arr = []
+#         visited = set()
+#         for i in range(len(s)):
+#             if s[i] in visited:
 #                 continue
-
-#             while arr and s[i] < arr[-1] and arr[-1] in s[i+1:]:
-#                 arr.pop()
-
-#             arr.append(s[i])
+#             while stack_Arr and s[i] < stack_Arr[-1] and dic[stack_Arr[-1]] > i:
+#                 visited.remove(stack_Arr[-1])
+#                 stack_Arr.pop()
+                
+#             stack_Arr.append(s[i])
+#             visited.add(s[i])
+#         return "".join(stack_Arr)
         
-#         return "".join(arr)
+        arr = []
+    
+        for i in range(len(s)):
+            if s[i] in arr:
+                continue
+
+            while arr and s[i] < arr[-1] and arr[-1] in s[i+1:]:
+                arr.pop()
+
+            arr.append(s[i])
+        
+        return "".join(arr)
                 
         
 #         visited = set()
