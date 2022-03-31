0ddc6386a7a593e3668afcd039964da95d92c66f
Jeet Mehta <47302941+JeetMehta99@users.noreply.github.com>
2022-03-30 19:22:39 -0500

    Time: 50 ms (75.67%), Space: 14.4 MB (46.10%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.py
@@ -1,43 +1,18 @@
-# class Solution:
-#     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-#         rows, cols = len(matrix), len(matrix[0])
-#         top = 0
-#         bottom = rows-1
-#         while top <= bottom: # Kinda like doing binary search
-#             row = (top+bottom)//2 #finding mid(BS)
-#             if target > matrix[row][-1]:
-#                 top = rows + 1
-#             elif target < matrix[row][0]:
-#                 bottom -= 1
-#             else:
-#                 break
-#         if not(top<=bottom): # top crosses bottom thus the element doesn't exist in the matrix
-#             return False
-#         row = (top + bottom) // 2 
-#         l, r = 0, cols - 1
-#         while l <= r:
-#             max1 = (l + r) // 2
-#             if target > matrix[row][max1]:
-#                 l = max1 + 1
-#             elif target < matrix[row][max1]:
-#                 r = max1 - 1
-#             else:
-#                 return True # element found
-#         return False # Element not found
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         rows, cols = len(matrix), len(matrix[0])
         
         top, bottom = 0, rows - 1
-        while top <= bottom:
-            row = (top + bottom) // 2
-            if target > matrix[row][-1]:
+        while top <= bottom: # Kinda like doing binary search
+            row = (top + bottom)//2  # finding mid(BS)
+            if target > matrix[row][-1]: # Run loop once to spot which half target number lies in
                 top = row + 1
             elif target < matrix[row][0]:
                 bottom = row - 1
-            else: break
+            else:
+                break
                 
-        if not (top <= bottom):
+        if not (top <= bottom): # top crosses bottom thus the element doesn't exist in the matrix
             return False
         
         row = (top + bottom) // 2
