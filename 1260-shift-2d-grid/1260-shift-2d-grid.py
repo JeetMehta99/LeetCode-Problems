6c4109ff399db7122207acd5e70e2afa38b42810
Jeet Mehta <47302941+JeetMehta99@users.noreply.github.com>
2022-04-12 23:20:28 -0500

    Time: 172 ms (86.92%), Space: 14.3 MB (64.41%) - LeetHub

---
+++ 1260-shift-2d-grid/1260-shift-2d-grid.py
@@ -0,0 +1,30 @@
+class Solution:
+    def shiftGrid(self, grid: List[List[int]], k: int) -> List[List[int]]:
+        r, c = len(grid), len(grid[0])
+        
+        k = k % (r*c) # no. of rotations
+
+        if k:
+            temp = []
+            i = ((r*c)-k)//c 
+            # print(i)
+            j = ((r*c)-k)%c
+            # print(j)
+            # r, c = divmod(m*n - k, n)
+            # r = r % m
+            while k:
+                if j==c:
+                    i+=1
+                    j=0
+                temp.append(grid[i][j])
+                j+=1
+                k-=1
+            pos = 0
+
+            for i in range(0, r):
+                for j in range(0, c):
+                    temp.append(grid[i][j])
+                    grid[i][j] = temp[pos] #rotation
+                    pos+=1
+        return grid
+        
