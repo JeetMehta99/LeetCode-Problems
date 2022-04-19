d3a94c29e8af74aa633b76b9a344bb37b2fe85a5
Jeet Mehta <47302941+JeetMehta99@users.noreply.github.com>
2022-04-19 02:54:49 -0500

    Time: 86 ms (66.04%), Space: 14.2 MB (65.76%) - LeetHub

---
+++ 99-recover-binary-search-tree/99-recover-binary-search-tree.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def recoverTree(self, root: Optional[TreeNode]) -> None:
+        """
+        Do not return anything, modify root in-place instead.
+        """
+        self.temp = []
+        def dfs(node):
+            if not node:
+                return 
+            dfs(node.left)
+            self.temp.append(node) # inorder
+            dfs(node.right)
+        dfs(root)
+        
+        sort = sorted(n.val for n in self.temp) # sort by vals
+        for i in range(len(sort)):
+            self.temp[i].val = sort[i]
