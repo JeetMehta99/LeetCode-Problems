bd90788acfa8c5d5fa9d521cca803733bfd2811e
Jeet Mehta <47302941+JeetMehta99@users.noreply.github.com>
2022-05-11 03:43:33 -0500

    Time: 53 ms (36.21%), Space: 13.9 MB (65.31%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.py
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.py
@@ -1,15 +1,31 @@
 class Solution:
     def countVowelStrings(self, n: int) -> int:
-        def recursion(n, k): # k: what val we are at inside of our string
-            if n == 0:
-                return 1
+        ## https://www.mathsisfun.com/combinatorics/combinations-permutations.html
+        return factorial(n+4)// factorial(n)// 24
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        ######### HORRIBLE TIME COMPLEXITY ##############
+        
+        #         def recursion(n, k): # k: what val we are at inside of our string
+#             if n == 0:
+#                 return 1
             
-            cnt = 0
-            for i in range(k, 5):
-                cnt += recursion(n-1, i) # avoid doing anything from before coz lexicography
+#             cnt = 0
+#             for i in range(k, 5):
+#                 cnt += recursion(n-1, i) # avoid doing anything from before coz lexicography
                 
-            return cnt
-        return recursion(n, 0)
-                
-                
-           
+#             return cnt
+#         return recursion(n, 0)
