1ccc2ed8b89b8cdce24dd82339b4a32de5cb6c13
Jeet Mehta <47302941+JeetMehta99@users.noreply.github.com>
2022-09-03 18:01:12 -0500

    Time: 97 ms (70.61%), Space: 14.2 MB (86.13%) - LeetHub

--- 1086-high-five/1086-high-five.py
+++ 1086-high-five/1086-high-five.py
@@ -4,17 +4,17 @@ from collections import defaultdict
 class Solution:
     def highFive(self, items: List[List[int]]) -> List[List[int]]:
         
-        cache = defaultdict(list)  # {student_id: [top-5 scores]}
-	
-        for student_id, score in items:
-            if len(cache[student_id]) < 5:  
-                heapq.heappush(cache[student_id], score)
+        cache = defaultdict(list)
+        
+        for s_id, score in items:
+            if(len(cache[s_id])) < 5:
+                heapq.heappush(cache[s_id], score)
             else:
-                heapq.heappushpop(cache[student_id], score)
-
-        ans = []
-        for student_id, scores in cache.items():
-            average = sum(scores) // len(scores)
-            ans.append((student_id, average))
-
-        return sorted(ans)
+                heapq.heappushpop(cache[s_id], score)
+        
+        res = []
+        for s_id, score in cache.items():
+            avg = sum(score) // len(score)
+            res.append((s_id, avg))
+        
+        return sorted(res)
