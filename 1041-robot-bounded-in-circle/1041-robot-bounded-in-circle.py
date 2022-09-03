4995bb9546fe6355bf19cc6b09bf118ec01b89b7
Jeet Mehta <47302941+JeetMehta99@users.noreply.github.com>
2022-09-03 13:47:23 -0500

    Time: 45 ms (57.07%), Space: 13.9 MB (22.21%) - LeetHub

--- 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.py
+++ 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.py
@@ -7,7 +7,7 @@ class Solution:
                 x, y = x+dirX, y+dirY
             elif direction == "L":
                 dirX, dirY = -1*dirY, dirX
-            else:
+            elif direction == "R":
                 dirX, dirY = dirY, -1*dirX
             
         return (x,y) == (0, 0) or (dirX, dirY) != (0, 1)
