51c90bc2a45f50baf58110c4b4a96d202c0dfd61
Jeet Mehta <47302941+JeetMehta99@users.noreply.github.com>
2022-09-07 22:46:28 -0500

    Time: 39 ms (90.39%), Space: 14.6 MB (67.38%) - LeetHub

---
+++ 430-flatten-a-multilevel-doubly-linked-list/430-flatten-a-multilevel-doubly-linked-list.py
@@ -0,0 +1,29 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val, prev, next, child):
+        self.val = val
+        self.prev = prev
+        self.next = next
+        self.child = child
+"""
+
+class Solution:
+    def flatten(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        if not head:
+            return None
+        
+        dummy = Node(0) # keep track of what head is
+        curr, stack = dummy, [head] 
+        while stack:
+            tmp = stack.pop()
+            if tmp.next:
+                stack.append(tmp.next)
+            if tmp.child:
+                stack.append(tmp.child)
+            curr.next = tmp
+            tmp.prev = curr
+            tmp.child = None
+            curr = tmp
+        dummy.next.prev = None
+        return dummy.next
