fb56a3cf56ed8cd09aef59483bfbebe0918c6a59
Jeet Mehta <47302941+JeetMehta99@users.noreply.github.com>
2022-09-29 00:49:07 -0500

    Time: 213 ms (5.92%), Space: 17.7 MB (29.23%) - LeetHub

--- 253-meeting-rooms-ii/253-meeting-rooms-ii.py
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.py
@@ -1,23 +1,10 @@
 from operator import itemgetter
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        # arr =[]
-        # finishTime = sorted(intervals)
-        # # print(finishTime)
-        # for i in finishTime:
-        #     # print(i[1])tt
-        #     if arr == [] or arr[0] > i[0]:
-        #         heapq.heappush(arr,i[1])
-        #         # print(arr)
-        #     else:
-        #         heapq.heapreplace(arr,i[1])
-        # return len(arr)
         first = itemgetter(0)
         second = itemgetter(1)
         start = sorted(intervals, key = first)
         end = sorted(intervals, key = second)
-        print(start)
-        print(end)
         res, count = 0, 0
         s, e = 0, 0
         while s < len(intervals):
@@ -29,4 +16,17 @@ class Solution:
                 count -= 1
             res = max(res, count)
         return res
+    
+    
+      # arr =[]
+        # finishTime = sorted(intervals)
+        # # print(finishTime)
+        # for i in finishTime:
+        #     # print(i[1])tt
+        #     if arr == [] or arr[0] > i[0]:
+        #         heapq.heappush(arr,i[1])
+        #         # print(arr)
+        #     else:
+        #         heapq.heapreplace(arr,i[1])
+        # return len(arr)
         
